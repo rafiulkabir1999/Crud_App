8b60c503ef4644ae4c1ad48a37b4380af935f825
unknown <rafi.eee.1999@gmail.com>
2024-07-07 12:51:41 +0600

    added api to router

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShowroomController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('get-showrooms',[ShowroomController::class,'getShowrooms']);
+Route::get('get-showrooms/{id}',[ShowroomController::class,'getShowroomsById']);
+Route::put('update-showrooms/{id}',[ShowroomController::class,'updateShowroom']);
+Route::delete('delete-showrooms/{id}',[ShowroomController::class,'deleteShowroom']);
